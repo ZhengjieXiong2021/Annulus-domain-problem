f15e30f14646b8e47766b7f64a76ab4728706194
ZhengjieXiong2021 <91766170+ZhengjieXiong2021@users.noreply.github.com>
2022-09-28 20:37:21 -0400

    Update besseltest.m

--- besseltest.m
+++ besseltest.m
@@ -2,11 +2,11 @@ function val = besseltest(m,lambda)
 % compute eqution g
 % m is the order of Bessel function and lambda is the eigenvalue we want to test
     if m==0
-        val=besselj(1,lambda).*bessely(1,0.5.*lambda)-bessely(1,lambda).*besselj(m,0.5.*lambda);
+        val=besselj(1,lambda).*bessely(1,0.5.*lambda)-bessely(1,lambda).*besselj(1,0.5.*lambda);
         val=abs(val);
     else
-        lhs=0.25.*(besselj(m-1,lambda)-besselj(m+1,lambda)).*(bessely(m-1,0.5*lambda)-bessely(m+1,0.5.*lambda));
-        rhs=0.25.*(besselj(m-1,0.5.*lambda)-besselj(m+1,0.5.*lambda)).*(bessely(m-1,lambda)-bessely(m+1,lambda));
+        lhs=0.125.*(besselj(m-1,lambda)-besselj(m+1,lambda)).*(bessely(m-1,0.5*lambda)-bessely(m+1,0.5.*lambda));
+        rhs=0.125.*(besselj(m-1,0.5.*lambda)-besselj(m+1,0.5.*lambda)).*(bessely(m-1,lambda)-bessely(m+1,lambda));
         val=abs(lhs-rhs);
     end
 end
